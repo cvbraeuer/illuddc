8079cf78cd8514301dd5d15b3c261e47955b88a8
Claudio Bräuer <contact@cvbraeuer.de>
2022-02-12 15:20:35 +0100

    Update illuddc.py
    some small changes, added smooth brightness transition

--- illuddc.py
+++ illuddc.py
@@ -35,30 +35,59 @@ ddcattr = "10"
 # verify TLS certificate? Set to false because of reasons
 verifyTLS = False
 
-
 headers = {'Authorization': 'Bearer ' + token, 'Content-Type': "application/json"}
 target = "https://" + host + "/api/states/" + sensor
 
 while True:
     # lazy catchall
     try:
-        response = requests.get(target, headers=headers, verify=verifyTLS).text
+        response = requests.get(target, headers=headers, verify=False).text
     except:
         continue
     # json to dict
     responseJson = json.loads(response)
     # for debugging purposes, print json to understand structure:
-    print(responseJson)
+    #print(responseJson)
     # set corresponding dictionary path to field here, e.g "attributes"-> "illuminance_lux"
     illuminance_cur = responseJson['attributes'][field]
+    
     # clamping the values to min or max
     if illuminance_cur <= illuminance_min:
         illuminance_cur = illuminance_min
     if illuminance_cur >= illuminance_max:
         illuminance_cur = illuminance_max
-    # linear interpolation
-    brightness = ((illuminance_cur - illuminance_min) / (illuminance_max - illuminance_min)) * (brightness_max - brightness_min) + brightness_min
+    
+    # linear interpolation for target value
+    brightness_target = int(((illuminance_cur - illuminance_min) / (illuminance_max - illuminance_min)) * (brightness_max - brightness_min) + brightness_min)
+    
+    # get current brightness
+    spargs = ['ddcutil', 'getvcp', ddcattr]
+    ret = subprocess.Popen(spargs, stdout=subprocess.PIPE)
+    output = ret.communicate()[0].decode('UTF-8').replace(" ","")
+    # validate output
+    try:
+        brightness_cur = int(re.findall("currentvalue=(\d+),", output)[0])
+    except:
+        continue
+
+    print(brightness_cur)
+    print(brightness_target)
+    
+    # do nothing if brightness stays the same
+    if brightness_cur == brightness_target:
+        time.sleep(interval)
+        continue
+
+    # smooth brightness transition
+    steps = brightness_target-brightness_cur
+    print(steps)
+    for i in range(0, abs(steps)):
+        brightness_cur += -1 if steps < 0 else 1
+        print(brightness_cur)
+        spargs = ['ddcutil', '--sleep-multiplier', '.1', 'setvcp', ddcattr, str(brightness_cur)]
+        subprocess.call(spargs)
+
+    #spargs = ['ddcutil', '--sleep-multiplier', '.5', 'setvcp', ddcattr, str(brightness_target)]
+    #subprocess.call(spargs)
     # set display brightness via ddcutil and go to sleep
-    spargs = ['ddcutil', 'setvcp', ddcattr, str(int(brightness))]
-    subprocess.call(spargs)
     time.sleep(interval)
